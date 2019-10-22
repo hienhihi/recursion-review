35409a105ef55b79d75455a8942d8a7c6456036e
hienhihi <emails.hienn@gmail.com>
2019-10-21 21:44:03 -0700

    semi works getelementbyclassname

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, element) {
   // your code here
+  var resArr = [];
+  var currentElement = element || document.body;
+
+  if (currentElement.classList) {
+    if (currentElement.classList.contains(className)) {
+      resArr.push(currentElement);
+    }
+  }
+
+  if (currentElement.childNodes.length !== 0) {
+    for (var i = 0; i < currentElement.childNodes.length; i++) {
+      getElementsByClassName(className, currentElement.childNodes[i]);
+    }
+  }
+
+  return resArr;
 };
